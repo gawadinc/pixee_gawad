ee6623115894aade6d69a8d6c2bf8c338a06ae85
gawad <gawad@ukr.net>
2023-11-26 15:41:04 +0100

    spring-javaformat:apply - done

--- src/main/java/org/springframework/samples/petclinic/charity/Charity.java
+++ src/main/java/org/springframework/samples/petclinic/charity/Charity.java
@@ -10,7 +10,6 @@ import org.springframework.samples.petclinic.model.NamedEntity;
 
 import java.time.LocalDate;
 
-
 @Entity
 @Table(name = "charity")
 public class Charity extends NamedEntity {
@@ -18,7 +17,6 @@ public class Charity extends NamedEntity {
 	@Column(name = "action_date")
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate actionDate;
-
 
 	@Column(name = "name")
 	private String name;
@@ -28,7 +26,6 @@ public class Charity extends NamedEntity {
 	@Digits(fraction = 0, integer = 10)
 	private String telephone;
 
-
 	public String getTelephone() {
 		return this.telephone;
 	}
@@ -37,12 +34,15 @@ public class Charity extends NamedEntity {
 		this.telephone = telephone;
 	}
 
+	@Override
+	public String getName() {
+		return name;
+	}
 
 	@Override
-	public String getName() { return name; }
-
-	@Override
-	public void setName(String name) { this.name = name; }
+	public void setName(String name) {
+		this.name = name;
+	}
 
 	public LocalDate getActionDate() {
 		return this.actionDate;
